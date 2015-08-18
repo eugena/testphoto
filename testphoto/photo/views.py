0abf64decebc7bccb6bfbf543a44f301d1e7abcc
eugena <eugena.mihailikova@gmail.com>
2015-08-18 19:33:13 +0500

    initial commit

--- testphoto/photo/views.py
+++ testphoto/photo/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from django.contrib.contenttypes.models import ContentType
 from django.views import generic
+
 
 from taggit.models import Tag, TaggedItem
 
@@ -19,13 +21,26 @@ class PhotoListView(generic.ListView):
         """
         slug = kwargs.get('slug')
 
+        # Conjunction
+        # qs = Photo.objects.filter(
+        #     pk__in=TaggedItem.objects.filter(
+        #         tag__in=Tag.objects.filter(slug__in=slug.split(',')),
+        #         tag__tag_state__state=1,
+        #         content_type=ContentType.objects.get_for_model(Photo.objects.model)
+        #     ).values_list("object_id", flat=True)
+        # )
+        tags = slug.split(',')
+
+        # Intersection
         qs = Photo.objects.filter(
-            pk__in=TaggedItem.objects.filter(
-                tag__in=Tag.objects.filter(slug__in=slug.split(',')),
-                tag__tag_state__state=1,
-                content_type=ContentType.objects.get_for_model(Photo.objects.model)
+                pk__in=TaggedItem.objects.values('object_id').filter(
+                        tag__in=Tag.objects.filter(slug__in=tags),
+                        tag__tag_state__state=1,
+                        content_type=ContentType.objects.get_for_model(Photo.objects.model)
+                ).annotate(cnt=Count('object_id')).filter(cnt=len(tags)
             ).values_list("object_id", flat=True)
         )
+
         if 'sort' in kwargs.keys():
             qs = qs.order_by(kwargs.get('sort'))
         else:
