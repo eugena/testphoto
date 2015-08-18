cb8c2083a06b506cb823b5a907fb892cc1cb101a
eugena <eugena.mihailikova@gmail.com>
2015-08-18 17:05:39 +0500

    initial commit

--- testphoto/photo/urls.py
+++ testphoto/photo/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import tagged_photo_list, PhotoListView
+from .views import PhotoListView
 
 
 urlpatterns = [
